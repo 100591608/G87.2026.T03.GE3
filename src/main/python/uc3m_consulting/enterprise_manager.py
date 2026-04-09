cefaaa335c1bc63de968d2a3d4de7d4e17af07b4
100578087 <100578087@alumnos.uc3m.es>
2026-04-09 20:31:18 +0200

    Refactored mysterious variable "c" to "cif_number"

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -17,17 +17,17 @@ class EnterpriseManager:
         pass
 
     @staticmethod
-    def validate_cif(c: str):
+    def validate_cif(cif_number: str):
         """validates a cif number """
-        if not isinstance(c, str):
+        if not isinstance(cif_number, str):
             raise EnterpriseManagementException("CIF code must be a string")
         p = re.compile(r"^[ABCDEFGHJKNPQRSUVW]\d{7}[0-9A-J]$")
-        if not p.fullmatch(c):
+        if not p.fullmatch(cif_number):
             raise EnterpriseManagementException("Invalid CIF format")
 
-        l = c[0]
-        n = c[1:8]
-        u = c[8]
+        l = cif_number[0]
+        n = cif_number[1:8]
+        u = cif_number[8]
 
         s1 = 0
         s2 = 0
