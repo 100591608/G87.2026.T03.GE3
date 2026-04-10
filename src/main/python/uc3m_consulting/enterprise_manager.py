87f90bd2437fca7be6b957ddc15ee9a693ceb5b8
100578087 <100578087@alumnos.uc3m.es>
2026-04-10 13:08:08 +0200

    Refactored mysterious variable "res" to "is_match" in validate_starting_date

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -64,8 +64,8 @@ class EnterpriseManager:
     def validate_starting_date(self, target_date):
         """validates the  date format  using regex"""
         date_pattern = re.compile(r"^(([0-2]\d|3[0-1])\/(0\d|1[0-2])\/\d\d\d\d)$")
-        res = date_pattern.fullmatch(target_date)
-        if not res:
+        is_match = date_pattern.fullmatch(target_date)
+        if not is_match:
             raise EnterpriseManagementException("Invalid date format")
 
         try:
