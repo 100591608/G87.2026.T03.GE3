179821e094f4ba559c0df2a8ba1a9f7826f0e16a
pablo-velarde <100591608@alumnos.uc3m.es>
2026-04-16 16:02:35 +0200

    Fix attributes.py class

--- src/main/python/uc3m_consulting/attributes/attributes.py
+++ src/main/python/uc3m_consulting/attributes/attributes.py
@@ -5,8 +5,8 @@ from uc3m_consulting import EnterpriseManagementException
 class Attributes():
     """Attributes generic class"""
     def __init__(self):
-        self.__attr_value = ""
-        self.__error_message = ""
+        self._attr_value = ""
+        self._error_message = ""
         self._validation_pattern = r""
 
     def _validate(self, value):
@@ -14,15 +14,15 @@ class Attributes():
         my_regex = re.compile(self._validation_pattern)
         res = my_regex.fullmatch(value)
         if not res:
-            raise EnterpriseManagementException(self.__error_message)
+            raise EnterpriseManagementException(self._error_message)
         return value
 
     @property
     def value(self):
         """Returns the attribute value"""
-        return self.__attr_value
+        return self._attr_value
 
     @value.setter
     def value(self, attr_value):
         """Sets the attribute value"""
-        self.__attr_value = attr_value
+        self._attr_value = attr_value
