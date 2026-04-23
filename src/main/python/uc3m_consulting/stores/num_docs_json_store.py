c1b2994826bd9ca488cefe38ca6c5168f6fff95a
pablo-velarde <100591608@alumnos.uc3m.es>
2026-04-23 16:36:20 +0200

    Apply singleton pattern to NumDocsJsonStore.

--- src/main/python/uc3m_consulting/stores/num_docs_json_store.py
+++ src/main/python/uc3m_consulting/stores/num_docs_json_store.py
@@ -6,5 +6,21 @@ from uc3m_consulting.enterprise_manager_config import TEST_NUMDOCS_STORE_FILE
 class NumDocsJsonStore(JsonStoreMaster):
     """Class for managing the num docs store"""
 
-    def __init__(self):
-        super().__init__(TEST_NUMDOCS_STORE_FILE)
+    class __NumDocsJsonStore(JsonStoreMaster):
+        """Class for managing the num docs store"""
+
+        def __init__(self):
+            super().__init__(TEST_NUMDOCS_STORE_FILE)
+
+    instance = None
+
+    def __new__(cls):
+        if not NumDocsJsonStore.instance:
+            NumDocsJsonStore.instance = NumDocsJsonStore.__NumDocsJsonStore()
+        return NumDocsJsonStore.instance
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+    def __setattr__(self, name, value):
+        return setattr(self.instance, name, value)
