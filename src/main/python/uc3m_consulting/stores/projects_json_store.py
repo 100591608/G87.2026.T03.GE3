37c0c0e0970fbb5addadcdec33926ad4c9273f54
pablo-velarde <100591608@alumnos.uc3m.es>
2026-04-23 16:38:37 +0200

    Apply singleton pattern to ProjectsJsonStore.

--- src/main/python/uc3m_consulting/stores/projects_json_store.py
+++ src/main/python/uc3m_consulting/stores/projects_json_store.py
@@ -7,12 +7,28 @@ from uc3m_consulting.enterprise_manager_config import PROJECTS_STORE_FILE
 class ProjectsJsonStore(JsonStoreMaster):
     """Class for managing the projects store"""
 
-    def __init__(self):
-        super().__init__(PROJECTS_STORE_FILE)
+    class __ProjectsJsonStore(JsonStoreMaster):
+        """Class for managing the projects store"""
 
-    def add_item_to_store(self, item_to_add):
-        self.load_store()
-        for project_item in self._data_list:
-            if project_item == item_to_add.to_json():
-                raise EnterpriseManagementException("Duplicated project in projects list")
-        super().add_item_to_store(item_to_add)
+        def __init__(self):
+            super().__init__(PROJECTS_STORE_FILE)
+
+        def add_item_to_store(self, item_to_add):
+            self.load_store()
+            for project_item in self._data_list:
+                if project_item == item_to_add.to_json():
+                    raise EnterpriseManagementException("Duplicated project in projects list")
+            super().add_item_to_store(item_to_add)
+
+    instance = None
+
+    def __new__(cls):
+        if not ProjectsJsonStore.instance:
+            ProjectsJsonStore.instance = ProjectsJsonStore.__ProjectsJsonStore()
+        return ProjectsJsonStore.instance
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+    def __setattr__(self, name, value):
+        return setattr(self.instance, name, value)
